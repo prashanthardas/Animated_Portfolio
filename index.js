e5a384b250d8a272f40ea446f9662b7ab335021d
prashanthardas <prashanthardas1997@gmail.com>
2023-02-21 17:58:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Mechanical Engineer", "Fresher Student", " Frontend Devloper", "Exp 'Html','Css','JS'"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
